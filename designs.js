c87cb3eb6196393573420ba088c2d15ed66d997c
Puneet Singh <37774803+puneet800123@users.noreply.github.com>
2018-04-08 21:41:41 +0530

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,26 @@
+canvas = $("#pixelCanvas");
+inputHeight = $("#inputHeight");
+inputWeight = $("#inputWeight");
+function makeGrid() {
+canvas.empty();
+height = inputHeight.val();
+width = inputWeight.val();
+  for ( rows = 0; rows < height; rows++) {
+  row = $("<tr></tr>");
+  canvas.append(row);
+
+    for (let columns = 0; columns < width; columns++) {
+      row.append("<td></td>");
+    }
+  }
+  $("td").on("click", function() {
+    color = $("#colorPicker").val();
+    $(this).css("background-color", color);
+});
+}
+
+$("#button").on("click", function(defaultsave) {
+  defaultsave.preventDefault();
+  makeGrid();
+});
+
