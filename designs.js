98843affa49236a59a09a3a85c57e8ff6ba68fcc
Puneet Singh <37774803+puneet800123@users.noreply.github.com>
2018-04-09 12:37:16 +0530

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,26 +1,29 @@
-canvas = $("#pixelCanvas");
-inputHeight = $("#inputHeight");
-inputWeight = $("#inputWeight");
-function makeGrid() {
-canvas.empty();
-height = inputHeight.val();
-width = inputWeight.val();
-  for ( rows = 0; rows < height; rows++) {
-  row = $("<tr></tr>");
-  canvas.append(row);
+//constants 
+const canvas = $("#pixelCanvas");
+const inputHeight = $("#inputHeight");
+const inputWeight = $("#inputWeight");
 
-    for (let columns = 0; columns < width; columns++) {
-      row.append("<td></td>");
+//function (making grid)
+function makeGrid() {
+    canvas.empty();
+    height = inputHeight.val();
+    width = inputWeight.val();
+    for (rows = 0; rows < height; rows++) {
+        row = $("<tr></tr>");
+        canvas.append(row);
+
+        for (let columns = 0; columns < width; columns++) {
+            row.append("<td></td>");
+        }
     }
-  }
-  $("td").on("click", function() {
-    color = $("#colorPicker").val();
-    $(this).css("background-color", color);
-});
+    $("td").on("click", function() {
+        color = $("#colorPicker").val();
+        $(this).css("background-color", color);
+    });
 }
 
+//button function
 $("#button").on("click", function(defaultsave) {
-  defaultsave.preventDefault();
-  makeGrid();
+    defaultsave.preventDefault();
+    makeGrid();
 });
-
